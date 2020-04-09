37a13bbe0eacd3f3788cd71d05fcb43c8f56c110
Sander Blue <sblue@newrelic.com>
2020-04-09 16:19:55 -0700

    chore: ci test 4:19

--- scripts/brew-formula.sh
+++ scripts/brew-formula.sh
@@ -1,3 +1,12 @@
+#!/usr/bin/env bash
+
+# Exit script if you try to use an uninitialized variable.
+set -o nounset
+# Exit script if a statement returns a non-true return value.
+set -o errexit
+# Use the error status of the first failure, rather than that of the last item in a pipeline.
+set -o pipefail
+
 sha256_result=$(openssl sha256 < ./dist/newrelic-cli_0.6.3-test_Darwin_x86_64.tar.gz)
 
 printf "\n"
