074dcc15f509d24deb6e49d57aa2f5f5196e0739
Sander Blue <sblue@newrelic.com>
2020-04-09 16:40:19 -0700

    chore: ci test - 4:40

--- scripts/brew-formula.sh
+++ scripts/brew-formula.sh
@@ -7,15 +7,22 @@ set -o errexit
 # Use the error status of the first failure, rather than that of the last item in a pipeline.
 set -o pipefail
 
+printf "\n"
 echo $PWD
+printf "\n"
 
 ls -la $PWD/dist
 
-# openssl sha256 < /home/circleci/project/dist/newrelic-cli_0.6.3-test_Darwin_x86_64.tar.gz
+asset_path="${PWD}/dist/newrelic-cli_${CIRCLE_TAG}_Darwin_x86_64.tar.gz"
+
+printf "\n"
+echo ${asset_path}
+printf "\n"
+
+openssl sha256 < ${asset_path}
 
 # sha256_result=$(openssl sha256 < /home/circleci/project/dist/newrelic-cli_0.6.3-test_Darwin_x86_64.tar.gz)
 
-printf "\n"
 # echo "SHA result: ${sha256_result}"
 
 printf "\n"
